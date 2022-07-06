f117033107887c789b18cc8a38e7f0a7c24853db
Matthias Arzt <arzt@mpi-cbg.de>
2022-07-06 09:43:43 +0200

    Refactor greeter_server

--- blender-addon/greeter_server.py
+++ blender-addon/greeter_server.py
@@ -57,27 +57,29 @@ class ManySpheres:
         sphere = self.reference_sphere.copy()
         sphere.name = request.id
         sphere.parent = self.parent_object
-        self.keyframe_set_visible(sphere, 0, False)
-        self.keyframe_set_visible(sphere, request.timepoints[0], True)
+        self.insert_visibility_keyframe(sphere, 0, False)
+        self.insert_visibility_keyframe(sphere, request.timepoints[0], True)
 
         last_time = 0
         for i in range(len(request.timepoints)):
-            time = request.timepoints[i];
-            sphere.location = (request.coordinates[i*3], request.coordinates[i*3 + 1], request.coordinates[i*3 + 2])
+            x = request.coordinates[i * 3]
+            y = request.coordinates[i * 3 + 1]
+            z = request.coordinates[i * 3 + 2]
+            time = request.timepoints[i]
+            sphere.location = ( x, y, z )
             sphere.keyframe_insert(data_path="location", frame=time)
             last_time = time
 
-        self.keyframe_set_visible(sphere, last_time + 1, False)
+        self.insert_visibility_keyframe(sphere, last_time + 1, False)
         self.collection.objects.link(sphere)
         return
 
     @staticmethod
-    def keyframe_set_visible(sphere, time, visible):
+    def insert_visibility_keyframe(sphere, time, visible):
         sphere.hide_viewport = not visible
         sphere.hide_render = not visible
         sphere.keyframe_insert(data_path="hide_viewport", frame=time)
         sphere.keyframe_insert(data_path="hide_render", frame=time)
-
 
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
 
