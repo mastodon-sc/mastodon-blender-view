33f5ad437ed4080af449e72ab7c71bc8f8de9aa2
Matthias Arzt <arzt@mpi-cbg.de>
2022-11-07 14:33:27 +0100

    Fix generate_code.sh

--- proto/generate_code.sh
+++ proto/generate_code.sh
@@ -2,5 +2,6 @@
 # python 3.9
 # conda install grpcio
 # conda install grpcio-tools
-python -m grpc_tools.protoc -I. --python_out=../blender-addon --grpc_python_out=../blender-addon mastodon-blender-view.proto
-sed -e "s/^import mastodon_blender_view_pb2 as mastodon__blender__view__pb2$/from . import mastodon_blender_view_pb2 as mastodon__blender__view__pb2/" -i ../blender-addon/mastodon_blender_view_pb2_grpc.py
+folder=mastodon-plugin/src/main/resources/blender-addon
+python -m grpc_tools.protoc -I. --python_out=../${folder} --grpc_python_out=../${folder} mastodon-blender-view.proto
+sed -e "s/^import mastodon_blender_view_pb2 as mastodon__blender__view__pb2$/from . import mastodon_blender_view_pb2 as mastodon__blender__view__pb2/" -i ../${folder}/mastodon_blender_view_pb2_grpc.py
